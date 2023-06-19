a90f91b486beb8b52f68c0cc9c642439aa5cc857
jamesmurdza <james@jamesmurdza.com>
2023-06-19 18:28:55 +0000

    Add addDatePicker transformation.

---
+++ src/addDatePicker.test.ts
@@ -0,0 +1,62 @@
+import j from 'jscodeshift'
+import { addDatePicker } from "./addDatePicker";
+import { toString } from "./utils";
+
+//
+// The transformation
+//
+
+const transformation = addDatePicker;
+const transformationName = "Add Date picker";
+
+//
+// The input of the transformation
+//
+
+const root = j(`\
+export default function App() {
+  return (<div><h1>Hello world</h1></div>)
+}\
+`);
+
+//
+// The expected output of the transformation
+//
+
+const expected = j(`\
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleDateChange}
+        dateFormat="dd/MM/yyyy"
+      />
+    </div>
+  );
+}\
+`)
+
+//
+// Check that the transformation works as expected
+//
+describe(transformationName, () => {
+  test("Check AST transformation", () => {
+    expect(
+      toString(transformation(root))
+    ).toEqual(
+      toString(expected)
+    )
+  });
+});
---
+++ src/addDatePicker.ts
@@ -0,0 +1,51 @@
+import j from 'jscodeshift'
+
+const IMPORTS = `
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
+`
+
+const HOOKS = `
+const [selectedDate, setSelectedDate] = useState(null);
+
+const handleDateChange = (date) => {
+  setSelectedDate(date);
+};
+`;
+
+const COMPONENT = `
+<DatePicker
+  selected={selectedDate}
+  onChange={handleDateChange}
+  dateFormat="dd/MM/yyyy"
+/>
+`;
+
+export const addDatePicker = (root: j.Collection) => {
+
+  // Find existing imports
+  const importDeclaration = root.find(j.ImportDeclaration);
+  // Insert the imports after the last existing import
+  if (importDeclaration.length) {
+    importDeclaration.at(-1).insertAfter(IMPORTS);
+  }
+  // Add the imports to the beginnning of the file
+  else {
+    root.find(j.Program).get('body', 0).insertBefore(IMPORTS);
+  }
+
+  // Find the default export
+  const appExport = root.find(j.ExportDefaultDeclaration);
+  const returnStament = appExport.find(j.ReturnStatement);
+
+  // Insert the hooks before the return statement
+  returnStament.at(0).insertBefore(HOOKS);
+
+  // Insert the new component add the end of the first JSX element
+  returnStament.find(j.JSXElement).at(0)
+    .childNodes().at(-1)
+    .insertAfter(COMPONENT);
+
+  return root;
+};
