3f08f56f642084e02b4ad5a9097e88e78e4ef8bf
jamesmurdza <james@jamesmurdza.com>
2023-06-19 17:05:38 +0000

    Improve test formatting.

--- src/addReactRouter.test.ts
+++ src/addReactRouter.test.ts
@@ -2,15 +2,28 @@ import j from 'jscodeshift'
 import { addReactRouter } from "./addReactRouter";
 import { toString } from "./utils";
 
-describe("Simple expression tests", () => {
-  test("Check literal value", () => {
-    const root = j(`\
+//
+// The transformation
+//
+
+const transformation = addReactRouter;
+const transformationName = "Add React router";
+
+//
+// The input of the transformation
+//
+
+const root = j(`\
 export default function App() {
   return <h1>Hello friend</h1>
 }\
 `);
-    const transformed = addReactRouter(root);
-    const expected = j(`\
+
+//
+// The expected output of the transformation
+//
+
+const expected = j(`\
 import { BrowserRouter, Route, Router, Switch } from "react-router-dom";
 
 function Home() {
@@ -25,6 +38,16 @@ export default function App() {
   return <Router><Switch><Route exact={true} path={"/"} component={Home}></Route><Route path={"/about"} component={About}></Route></Switch></Router>;
 }\
 `)
-    expect(toString(transformed)).toEqual(toString(expected))
+
+//
+// Check that the transformation works as expected
+//
+describe(transformationName, () => {
+  test("Check AST transformation", () => {
+    expect(
+      toString(transformation(root))
+    ).toEqual(
+      toString(expected)
+    )
   });
 });
