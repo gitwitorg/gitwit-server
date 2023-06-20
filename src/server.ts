55d9ed7a93f30104828ae3bf628e42574262bb40
James Murdza <james@jamesmurdza.com>
2023-06-20 09:43:02 -0700

    Add webserver.

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from 'express';
+import { transformFiles } from './transform'
+import cors from 'cors'
+
+// Create an Express application
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Define a route handler for the root path
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, World!');
+});
+
+// Define an API endpoint for code transformation
+app.post("/transform", async (req: Request, res: Response) => {
+    try {
+        console.log(req.body)
+        const modifiedCode = transformFiles(req.body.files, req.body.transformation);
+        res.json(modifiedCode);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error });
+    }
+});
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
