500312d40cf3b47232949820c1cc67519194d810
James Murdza <james@jamesmurdza.com>
2023-06-20 13:18:16 -0700

    Clean up.

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,23 @@
 import prettier from 'prettier'
 import j from 'jscodeshift'
 
-// Utility function to compare two ASTs
-export const toString = (ast: j.Collection) => prettier.format(
-  ast.toSource(), { parser: 'babel' }
-)
-
 // Format the modified source code with ESLint or Prettier
 export const formatCode = (code: string) =>
   prettier.format(code, {
     parser: 'babel'
   })
 
+// Utility function to compare two ASTs
+export const toString = (ast: j.Collection) => formatCode(
+  ast.toSource()
+)
+
 // Apply an AST transformation to the contents of a file
-export const applyTransform = (code: string, transform: Function) => formatCode(transform(j(code)).toSource())
+export const applyTransform = (code: string, transform: Function) =>
+  formatCode(
+    transform(j(code))
+      .toSource()
+  )
 
 // Add a dependency to a package.json file
 export const addDependency = (code: string, dependency: string, version: string) => {
